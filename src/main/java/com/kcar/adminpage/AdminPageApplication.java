d6fabfb26de8c8fce6a33d862ae538f0bb9ce2a7
Hyeonw <hyeonw777@naver.com>
2022-06-01 10:54:00 +0900

    환경구축 완료

---
+++ src/main/java/com/kcar/adminpage/AdminPageApplication.java
@@ -0,0 +1,13 @@
+package com.kcar.adminpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminPageApplication.class, args);
+	}
+
+}
